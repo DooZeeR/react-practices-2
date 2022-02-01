5f1baa62a84511b4708813bbbcfd91b2728d74aa
DooZeeR <zsolt.ordas@gmail.com>
2022-02-01 13:26:57 +0100

    12 week begin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
-import ButtonWithClass from './ButtonWithClass'
-import ButtonWithHooks from './ButtonWithHooks'
+/* import ButtonWithClass from './ButtonWithClass'
+import ButtonWithHooks from './ButtonWithHooks' */
 
 
 
 function App() {
   return (
     <div className="App">
-      <ButtonWithClass />
-      <ButtonWithHooks />
+      <h1>React App</h1>
+{/*       <ButtonWithClass />
+      <ButtonWithHooks /> */}
     </div>
   );
 }
